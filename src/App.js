a7c4be8de14af0413366019d407d4b2692fa3537
pedro-cal <pedroluna2@hotmail.com>
2022-09-23 11:35:44 -0300

    First commit - useState examples

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+// const arrayExample = ['Diego', 'Malu', 'Alany', 'Vinicius', 'Primo'];
+// console.log(arrayExample);
+
+// const [,menina1,menina2,,] = arrayExample;
+// console.log(menina1);
+// console.log(menina2);
+// const [one,two,three,four,five] = arrayExample;
+// console.log(four);
+
 function App() {
+   const [flamengoScore, setFlamengoScore] = useState(0);
+   const [botafogoScore, setBotafogoScore] = useState('teste');
+
+   function handleIncreaseFlamengo() {
+      setFlamengoScore(flamengoScore + 1);
+   }
+   function handleIncreaseBotafogo() {
+      setBotafogoScore(botafogoScore + 1);
+   }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <div style={{ display: 'flex' }}>
+            <div>
+               <button
+                  onClick={handleIncreaseFlamengo}
+               >
+                  Flamengo
+               </button>
+               <p>{flamengoScore}</p>            
+            </div>
+            <div>
+               <button
+                  onClick={handleIncreaseBotafogo}
+               >
+                  Botafogo
+               </button>
+               <p>{botafogoScore}</p>            
+            </div>
+         </div>
       </header>
     </div>
   );
